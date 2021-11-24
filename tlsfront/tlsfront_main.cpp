94558c4781154c407cab1ab6b9239aa05ae255fd
shpal2000 <shpal2000@gmail.com>
2021-11-24 01:04:37 +0000

    deployment files

--- tlsfront/tlsfront_main.cpp
+++ tlsfront/tlsfront_main.cpp
@@ -1,29 +1,38 @@
 
 #include "tlsfront_app.hpp"
 
-int main(int /*argc*/, char ** argv)
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+int main(int /*argc*/, char ** /*argv*/)
 {
     signal(SIGPIPE, SIG_IGN);
+
+    char* cfg_file = "/configs/config.json";
+
+    std::ifstream cfg_stream(cfg_file);
+    json cfg_json = json::parse(cfg_stream);
 
     std::vector<tlspack_app*> app_list;
     std::vector<tlspack_app_stats*> stats_list;
 
-    tlsfront_cfg ssl_app_cfg;
-    tlsfront_stats ssl_app_gstats;
+    tlsfront_cfg app_cfg;
+    tlsfront_stats app_gstats;
 
-    ssl_app_cfg.front_ip = argv[1];
-    ssl_app_cfg.front_port = std::stoi(argv[2]);
+    app_cfg.front_ip = cfg_json["front_ip"].get<std::string>();
+    app_cfg.front_port = cfg_json["front_ip"].get<int>();
 
-    ssl_app_cfg.server_ip = argv[3];
-    ssl_app_cfg.server_port =  std::stoi(argv[4]);
+    app_cfg.server_ip = cfg_json["server_ip"].get<std::string>();
+    app_cfg.server_port = cfg_json["server_ip"].get<int>();
 
-    ssl_app_cfg.back_ip = argv[5];
-    
+    app_cfg.back_ip = cfg_json["back_ip"].get<std::string>();
+
+
     tlsfront_app* tcpApp 
-        = new tlsfront_app(&ssl_app_cfg, &ssl_app_gstats);
+        = new tlsfront_app(&app_cfg, &app_gstats);
 
     app_list.push_back(tcpApp);
-    stats_list.push_back(&ssl_app_gstats);
+    stats_list.push_back(&app_gstats);
 
     tlspack_app::RunLoop(&app_list, &stats_list);
 
