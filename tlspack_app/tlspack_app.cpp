1521878e3afb9fd657b85fad3e23b30f65194181
shpal2000 <shpal2000@gmail.com>
2021-11-27 04:48:28 +0000

    refactor

--- tlspack_app/tlspack_app.cpp
+++ tlspack_app/tlspack_app.cpp
@@ -19,19 +19,21 @@ void tlspack_app::run_iter(bool tick_sec)
 void tlspack_app::RunLoop(std::vector<tlspack_app*> *app_list
                     , std::vector<tlspack_app_stats*> *stats_list)
 {
-    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::time_point<std::chrono::system_clock> start, current;
     start = std::chrono::system_clock::now();
     bool is_tick_sec = false;
     while (true)
     {
         std::this_thread::sleep_for(std::chrono::microseconds(1));
 
+        current = std::chrono::system_clock::now();
+
         auto ms_elapsed 
-         = std::chrono::duration_cast<std::chrono::milliseconds> (end-start);
+         = std::chrono::duration_cast<std::chrono::milliseconds> (current-start);
 
         if (ms_elapsed.count() >= 1000)
         {
-            start = end;
+            start = current;
             is_tick_sec = true;
         }
 
