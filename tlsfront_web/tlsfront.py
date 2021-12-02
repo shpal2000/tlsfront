2f91945a54604da8857c4bd323180196bb7f16fe
shpal2000 <shpal2000@gmail.com>
2021-12-02 07:57:27 +0000

    web interface

---
+++ tlsfront_web/tlsfront.py
@@ -0,0 +1,15 @@
+import os
+import aiohttp, json
+from aiohttp import web
+
+async def index_handle(request):
+    return web.FileResponse('public/index.html')
+
+app = web.Application()
+
+app.add_routes([web.route('get', '/{tail:.*}', index_handle)])
+
+
+if __name__ == '__main__':  
+    web.run_app(app, port=8888)
+
