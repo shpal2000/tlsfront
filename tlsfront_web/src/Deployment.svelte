2f1b075936ed5af3df6d80b761ea94d4d91b2cde
shpal2000 <shpal2000@gmail.com>
2021-12-13 02:05:50 +0000

    webui

--- tlsfront_web/src/Deployment.svelte
+++ tlsfront_web/src/Deployment.svelte
@@ -1,7 +1,38 @@
 <script>
     import { onMount } from 'svelte';
+    import Line from "svelte-chartjs/src/Line.svelte"
 
     let deployments = {};
+    let data = {
+        labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+        datasets: [{ 
+        data: [86,114,106,106,107,111,133,221,783,2478],
+        label: "Africa",
+        borderColor: "#3e95cd",
+        fill: false
+      }, { 
+        data: [282,350,411,502,635,809,947,1402,3700,5267],
+        label: "Asia",
+        borderColor: "#8e5ea2",
+        fill: false
+      }, { 
+        data: [168,170,178,190,203,276,408,547,675,734],
+        label: "Europe",
+        borderColor: "#3cba9f",
+        fill: false
+      }, { 
+        data: [40,20,10,16,24,38,74,167,508,784],
+        label: "Latin America",
+        borderColor: "#e8c3b9",
+        fill: false
+      }, { 
+        data: [6,3,2,2,7,26,82,172,312,433],
+        label: "North America",
+        borderColor: "#c45850",
+        fill: false
+      }
+    ]
+    }
 
 	onMount(() => {
 		const interval = setInterval(() => {
@@ -9,6 +40,7 @@
                 .then((response) => response.json())
                 .then((results) => {
                     deployments = results;
+
                 });
 		}, 1000);
 
@@ -51,6 +83,12 @@
     {/each}
       </tbody>
 </table>
+
+<Line
+  data={data}
+  width={50}
+  height={25}
+/>
 
 
 <style>
